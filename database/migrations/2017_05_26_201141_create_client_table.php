487a053b8f6cf8890dbf4f97c2022a96d18e4676
Leila <leila.lazo@tektonlabs.com>
2017-05-26 21:14:45 +0000

    migrations, benefits model and seed

--- database/migrations/2017_05_26_201141_create_client_table.php
+++ database/migrations/2017_05_26_201141_create_client_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateClientTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->integer('user_type_id')->unsigned();
+            $table->string('name');
+            $table->string('lastname');
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
 
-            $table->foreign('user_type_id')->references('id')->on('user_types');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -33,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('clients');
     }
 }
