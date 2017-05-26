724916074aa993bef94bfc5edfe70f68f9704aae
Leila <leila.lazo@tektonlabs.com>
2017-05-26 22:08:02 +0000

    typo

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -28,7 +28,7 @@ $factory->define(App\Models\Benefit::class, function (Faker\Generator $faker) {
         'title' => $faker->text(50),
         'description' => $faker->paragraph(),
         'image_url' => 'http://institutoavanzadodecolumna.es/wp-content/uploads/2016/06/image.jpeg',
-        'due_date' => faker->dateTimeBetween('now','+30 days'),
+        'due_date' => $faker->dateTimeBetween('now','+30 days'),
         'place' => $faker->text(15),
         'points' => $faker->numberBetween(10,99),
 //        'qr_file' => url()->full().'/qr/qrimage.png',
