3e23e181a6faa7a1ebe8cb2b69968f17dfdf9a57
Janet Sarmiento <janet.sarmiento@tektonlabs.com>
2017-05-27 10:27:08 +0000

    Added last card in dashboard

--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -64,12 +64,51 @@
           </div>
         </div>
       </div>
-      <div>
+      <div class="dashboard-cards-container">
         <div class="dashboard-card-linear-chart-container">
           <p class="dashboard-card-information-text">
             Histórico de Registros
           </p>
-          <canvas class="dashboard-card-linera-chart" id="linear-chart" width="400" height="250"></canvas>
+          <canvas class="dashboard-card-liner-chart" id="linear-chart" width="400" height="250"></canvas>
+        </div>
+        <div class="dashboard-card">
+          <div class="dashboard-card-wrapper-helper">
+            <div class="dashboard-card-information-wrapper">
+              <p class="dashboard-card-information-text">
+                Edades
+              </p>
+              <div class="dashboard-card-donut-chart-wrapper">
+                <canvas class="dashboard-card-donut-chart" id="donut-chart" width="90" height="90"></canvas>
+              </div>
+              <div class="dashboard-card-chart-legend-container">
+                <span class="is-pink">&#9899</span>
+                <span class="dashboard-card-chart-legend">18-22</span>
+                <span class="is-blue">&#9899</span>
+                <span class="dashboard-card-chart-legend">23-27</span>
+                <span class="is-green">&#9899</span>
+                <span class="dashboard-card-chart-legend">28+</span>
+              </div>
+              <p class="dashboard-card-information-text">
+                Top Districts
+              </p>
+              <ol class="dashboard-card-numeric-list">
+                <li>Jesús María</li>
+                <li>Magdalena</li>
+                <li>Surco</li>
+                <li>Pueblo Libre</li>
+              </ol>
+              <p class="dashboard-card-information-text">
+                People
+              </p>
+              <div id="gender-progress-bar" class="dashboard-card-information-progressbar"></div>
+              <div class="dashboard-card-chart-legend-container">
+                <span class="is-blue">&#9899</span>
+                <span class="dashboard-card-chart-legend">Hombres</span>
+                <span class="is-green">&#9899</span>
+                <span class="dashboard-card-chart-legend">Mujeres</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -132,25 +171,51 @@
           mode: 'nearest',
           intersect: true
         },
-        /*scales: {
-          xAxes: [{
-              display: true,
-              scaleLabel: {
-                  display: true,
-                  labelString: 'Month'
-              }
-          }],
-          yAxes: [{
-              display: true,
-              scaleLabel: {
-                  display: true,
-                  labelString: 'Value'
-              }
-          }]
-        }*/
       },
     };
 
     var linearChartGraphic = new Chart(linearChart, config);
+
+    var donutChart = document.getElementById("donut-chart");
+    var configAges = {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: [
+                    12,
+                    45,
+                    9
+                ],
+                backgroundColor: [
+                    '#ff6d8a',
+                    '#6d8aff',
+                    '#64e283'
+                ],
+                borderWidth: [ 0, 0, 0]
+            }],
+        },
+        options: {
+            responsive: true,
+            legend: {
+                position: 'bottom',
+            },
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            },
+        }
+    };
+    var donutChartGraphic = new Chart(donutChart, configAges);
+
+    var barGender = new ProgressBar.Line('#gender-progress-bar', {
+      strokeWidth: 4,
+      easing: 'bounce',
+      duration: 1400,
+      color: '#6d8aff',
+      trailColor: '#64e283',
+      trailWidth: 4,
+      svgStyle: {width: '100%', height: '100%', 'border-radius': '20px'}
+    });
+    barGender.animate(0.45);
   </script>
 @endsection
