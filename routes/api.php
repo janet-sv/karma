0da3d0c247f84650d64de99b3d38e65338221e1e
Your Name <leila.lazo@tektonlabs.com>
2017-05-27 05:31:04 +0000

    minor change in routes api

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'benefit', 'middleware' => 'api'], function(){
-    Route::get('list', 'BenefitController@index');
+Route::group(['prefix' => 'benefits', 'middleware' => 'api'], function(){
+    Route::get('/', 'BenefitController@index');
     Route::post('redeem', 'BenefitController@redeem');
 });
 
